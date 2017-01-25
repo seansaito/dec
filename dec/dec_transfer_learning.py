3c618159a1e1f8dcba8c254cc3fbc5edc4b74f7f
seansaito <saitosean@ymail.com>
2017-01-25 14:55:23 +0800

    Update

--- dec/dec_transfer_learning.py
+++ dec/dec_transfer_learning.py
@@ -177,11 +177,11 @@ def DisKmeans(db, update_interval = None):
                 device_id: 0
             """%(update_interval*100))
 
-        os.system('caffe train --solver=solver.prototxt --weights=init.caffemodel')
-        shutil.copyfile('exp/test/save_iter_%d.caffemodel'%update_interval, 'init.caffemodel')
-
         os.system('caffe train --solver=reconst_solver.prototxt --weights=init.caffemodel')
         shutil.copyfile('exp/mnist/save_iter_16000.caffemodel', 'init.caffemodel')
+
+        os.system('caffe train --solver=solver.prototxt --weights=init.caffemodel')
+        shutil.copyfile('exp/test/save_iter_%d.caffemodel'%update_interval, 'init.caffemodel')
 
         iters += 1
         seek = (seek + train_batch_size*update_interval)%N
